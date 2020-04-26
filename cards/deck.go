58056f0b50b93c9111080b931b07e60bf437e19d
Douglas Amorim <frc200_2@hotmail.com>
2020-04-26 02:03:52 -0300

    wip

--- cards/deck.go
+++ cards/deck.go
@@ -1,1 +1,14 @@
 package main
+
+import "fmt"
+
+// Create a new type of "deck"
+// which is a slice of strings
+type deck []string
+
+// d here are just the same as "this" in js or "self" in pyton
+func (d deck) print() {
+	for i, card := range d {
+		fmt.Println(i, card)
+	}
+}
--- cards/main.go
+++ cards/main.go
@@ -1,12 +1,10 @@
 package main
 
-import "fmt"
-
 func main() {
-	cards := []string{"Ace of Diamonds", newCard()}
+	cards := deck{"Ace of Diamonds", newCard()}
 	cards = append(cards, "Six of Spades")
 
-	fmt.Println(cards)
+	cards.print()
 }
 
 func newCard() string {
