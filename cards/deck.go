74ae6eebbd24bbbbcc387f97c1faa96465dba30f
Douglas Amorim <frc200_2@hotmail.com>
2020-04-27 01:09:59 -0300

    first testing done

--- cards/deck.go
+++ cards/deck.go
@@ -16,7 +16,7 @@ type deck []string
 func newDeck() deck {
 	cards := deck{}
 
-	cardSuits := []string{"Spades", "Diamods", "Hearts", "Clubs", "Diamonds"}
+	cardSuits := []string{"Spades", "Diamonds", "Hearts", "Clubs"}
 	cardValues := []string{"Ace", "Two", "Three", "Four"}
 
 	for _, suit := range cardSuits {
--- cards/deck_test.go
+++ cards/deck_test.go
@@ -1,1 +1,11 @@
 package main
+
+import "testing"
+
+func TestNewDeck (t *testing.T) {
+	d := newDeck()
+
+	if len(d) != 16 {
+		t.Errorf("Expected deck leght of 16, but got: %v ", len(d))
+	}
+}
