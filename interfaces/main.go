fa2d2cc848973da5625c1b51a79849f31181bea4
Douglas Amorim <frc200_2@hotmail.com>
2020-04-30 01:15:51 -0300

    bad way

--- interfaces/main.go
+++ interfaces/main.go
@@ -1,13 +1,33 @@
 package main
 
+import "fmt"
+
 type englishBot struct{}
 type spanishBot struct{}
 
 func main() {
+	eb := englishBot{}
+	sp := spanishBot{}
 
+	printGreeting(eb)
+	printGreeting(sb)
 }
 
-func (eb englishBot) getGreeting() string {
+func printGreeting (eb englishBot) {
+	fmt.Println(eb.getGreeting())
+}
 
+func printGreeting(sb spanishBot) {
+	fmt.Println(sb.getGreeting())
+}
+
+func (englishBot) getGreeting() string {
+	// VERY custom logic for generation an enlsh greeting
+	return "Hi There!"
+}
+
+func (spanishBot) getGreeting() string {
+	// VERY custom logic for generation an enlsh greeting
+	return "Hola!"
 }
 
