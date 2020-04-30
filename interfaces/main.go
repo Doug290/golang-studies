4b0e658d9250b741536f087a1a43f793a38a769c
Douglas Amorim <frc200_2@hotmail.com>
2020-04-30 01:19:43 -0300

    refactoring with interface

--- interfaces/main.go
+++ interfaces/main.go
@@ -2,23 +2,23 @@ package main
 
 import "fmt"
 
+type bot interface {
+	getGreeting() string
+}
+
 type englishBot struct{}
 type spanishBot struct{}
 
 func main() {
 	eb := englishBot{}
-	sp := spanishBot{}
+	sb := spanishBot{}
 
 	printGreeting(eb)
 	printGreeting(sb)
 }
 
-func printGreeting (eb englishBot) {
-	fmt.Println(eb.getGreeting())
-}
-
-func printGreeting(sb spanishBot) {
-	fmt.Println(sb.getGreeting())
+func printGreeting(b bot) {
+	fmt.Println(b.getGreeting())
 }
 
 func (englishBot) getGreeting() string {
